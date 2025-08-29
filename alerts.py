c438ac8ba44fdec7d43457fe2f9a7c53cda683d8
dwadesknnes <devinfraser05@gmail.com>
2025-08-28 22:02:49 -0400

    Create alerts.py with notify functions

---
+++ alerts.py
@@ -0,0 +1,4 @@
+# TODO: Write notify(text) that sends Telegram messages using TELEGRAM_BOT_TOKEN + TELEGRAM_CHAT_ID from env.
+# Also add notify_exception(label, e) that formats a traceback and calls notify.
+
+import requests
